d70e7f6d52f54c84a819cc7289400b4aea5157c2
nusratjahandina3789 <dinujahan@gmail.com>
2025-06-30 21:41:22 +0100

    javascript

---
+++ script.js
@@ -0,0 +1,117 @@
+//Array unshift method
+// const studentNameone = ["Dina","Santo","Nusrat","Sahan"];
+// console.log (studentNameone);
+// const newArrone = studentNameone.unshift("Jahan");
+// console.log(studentNameone);
+
+//Array shift method
+// const studentNametwo = ["Dina","Santo","Nusrat","Sahan"];
+// console.log (studentNametwo);
+// const newArrtwo = studentNametwo.shift();
+// console.log(studentNametwo);
+
+//Array push method
+// const studentNamethree = ["Dina","Santo","Nusrat","Sahan"];
+// console.log (studentNamethree);
+// const newArrthree = studentNamethree.push("Jahan");
+// console.log(studentNamethree);
+
+//Array pop method
+// const studentNamefour = ["Dina","Santo","Nusrat","Sahan"];
+// console.log (studentNamefour);
+// const newArrfour = studentNamefour.pop();
+// console.log(studentNamefour);
+
+// Function Declaration
+// function greeting() {
+//     console.log("Hello!");
+// }
+// greeting();
+
+// Function Expression
+// const result = function greeting() {
+//     console.log("Hello!");
+// }
+// result();
+
+
+// Arrow Function 
+// const result = ()=> {
+//     console.log("Hello!");
+// }
+// result();
+
+
+// IIFE Function 
+// (function (){
+//     console.log("Hello Mern 2408");
+// })();
+
+// let demo = (function (){
+//     console.log("Hello Mern 2408");
+// })();
+
+// function addNum (a,b) {
+//     console.log(a + b);
+// }
+// addNum(10, 5);
+
+// Return Keyword
+// function addNum (a,b) {
+//     return a + b;
+// }
+// const result = addNum(10, 5);
+// console.log(result);
+
+// Nested Object 
+// const studentInfo = {
+//     studentName : "Nusrat Jahan Dina",
+//     studentId: "10",
+//     studentAge : "20",
+//     studentAddress : {
+//         studentDivision : "Chittagong",
+//         studentDistrict : "Chandpur",
+//         studentThana : "Chandpur",
+//     },
+// };
+// console.log(studentInfo.studentAddress.studentDistrict);
+
+// Method
+// const studentInfo = {
+//     studentName : "Nusrat Jahan Dina",
+//     studentId: "10",
+//     studentAge : "20",
+//     studentAddress : {
+//         studentDivision : "Chittagong",
+//         studentDistrict : "Chandpur",
+//         studentThana : "Chandpur",
+//     },
+//     greeting : function () {
+//         console.log("Hello Mern 2408")
+//     },
+// };
+// console.log(studentInfo.greeting());
+
+
+// this keyword
+// const person = {
+//     firstName : "Dina",
+//     lastName : "Munshi",
+//     id : "10",
+//     fullName : function () {
+//         return this.firstName + " " + this.lastName;
+//     },
+// };
+// console.log(person.fullName());
+
+// const country = "Bangladesh";
+// console.log("My country name is "+ country + " I love " + country);
+// console.log(`My country name is ${country}. I love my country ${country}.`);
+
+
+// const country = {
+//       countryName : "Bangladesh",
+//       countryAge : "60",
+// };
+// console.log(`My country name is ${country.countryName}. My country age is ${country.countryAge}.`);
+
