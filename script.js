89b4894eb62c2d6991f00804727035376d60cc50
nusratjahandina3789 <dinujahan@gmail.com>
2025-07-07 19:43:08 +0600

    namta

--- script.js
+++ script.js
@@ -115,28 +115,51 @@
 // };
 // console.log(`My country name is ${country.countryName}. My country age is ${country.countryAge}.`);
 
-const myMark = 83;
-const schoolarshipMark = 81;
-if (myMark < 32) {
-    console.log("My result is F grade. \n");
-} else if (myMark >= 33 && myMark < 39) {
-    console.log("My result is D grade. \n");
-} else if (myMark >= 40 && myMark < 49) {
-    console.log("My result is C grade. \n");
-} else if (myMark >= 50 && myMark < 59) {
-    console.log("My result is B grade. \n");
-} else if (myMark >= 60 && myMark < 69) {
-    console.log("My result is A- grade. \n");
-} else if (myMark >= 70 && myMark < 79) {
-    console.log("My result is A grade. \n");
-} else if (myMark >= 80 && myMark <= 100) {
-    console.log("My result is A+ grade. \n");
-    if (myMark >= schoolarshipMark) {
-        console.log("I got schoolarship. \n");
-    } else {
-        console.log("I'm not eligible for schoolarship. \n");
-    }
-}
-else {
-    console.log("Result not found! \n");
-}
+// const myMark = 100;
+// const schoolarshipMark = 81;
+// if (myMark < 0) {
+//     console.log("Please enter decimal number. \n");
+// } else if(myMark < 32){
+//    console.log("My result is F grade.\n");
+// }else if (myMark >= 33 && myMark < 39) {
+//     console.log("My result is D grade. \n");
+// } else if (myMark >= 40 && myMark < 49) {
+//     console.log("My result is C grade. \n");
+// } else if (myMark >= 50 && myMark < 59) {
+//     console.log("My result is B grade. \n");
+// } else if (myMark >= 60 && myMark < 69) {
+//     console.log("My result is A- grade. \n");
+// } else if (myMark >= 70 && myMark < 79) {
+//     console.log("My result is A grade. \n");
+// } else if(myMark > 100){
+//    console.log("Please enter 0 to 100 number. \n");
+// }else if (myMark >= 80 && myMark <= 100) {
+//     console.log("My result is A+ grade. \n");
+//     if (myMark >= schoolarshipMark) {
+//         console.log("I got schoolarship. \n");
+//     } else {
+//         console.log("I'm not eligible for schoolarship. \n");
+//     }
+// }
+// else {
+//     console.log("Result not found! \n");
+// }
+
+// -1 to -10 print in for loop
+// for( var i = -1; i >= -10; i--) {
+//     console.log(i)
+// }
+
+// -1 to -10 print in while loop
+// var i = -1;
+// while( i >= -10){
+//     console.log(i);
+//     i--;
+// }
+// 1 to 5 namta in nested loop
+//  var number = 5;
+//  for(var i = 1; i<= number; i++){
+//  for(var j = 1; j<= 10; j++){
+//     document.write(i + " X " + j +" = " + i*j + "</br>");
+//  }
+//  }
